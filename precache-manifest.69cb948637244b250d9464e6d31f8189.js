169b954a36062874f007b15e9e45639fa1ad820e
aljoker2001 <aljoker2001@hotmail.com>
2019-07-07 18:41:40 -0700

    Updates

---
+++ precache-manifest.69cb948637244b250d9464e6d31f8189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c32ea158e9330a3e0d804a219801bd",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "27e2f503ea563ebb5df3",
+    "url": "/ClickyGame/static/css/main.1da60b61.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ClickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "27e2f503ea563ebb5df3",
+    "url": "/ClickyGame/static/js/main.0580df01.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "37ba67e42fe0596d1263e4e85f2387ec",
+    "url": "/ClickyGame/static/media/jay-dkFJST9zZZo-unsplash.37ba67e4.jpg"
+  }
+]);
